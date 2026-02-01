e79c6dbe0f8e45bbc4536d6c59f7c162e6ecf717
Shefat Al Mahmud <shefat2002@gmail.com>
2026-02-01 08:33:50 +0600

    created grpc initial service

--- BeziqueCore.Tests/BeziqueGameApiTests.cs
+++ BeziqueCore.Tests/BeziqueGameApiTests.cs
@@ -10,7 +10,7 @@ public class BeziqueGameApiTests
     {
         public void InitializeGame()
         {
-                   
+            
         }
 
         public void NotifyGameInitialized()
--- GrpcService/Services/BeziqueService.cs
+++ GrpcService/Services/BeziqueService.cs
@@ -7,12 +7,12 @@ public class BeziqueService : Bezique.BeziqueBase
     public override Task<GameInitReply> GameInit(CreateRequest request, ServerCallContext context)
     {
         var gameInitReply = new GameInitReply();
-        var is2Player = request.PlayerCount == 2;
-        var is4Player = request.PlayerCount == 4;
-        if (is2Player == false && is4Player == false)
-        {
-            gameInitReply.Message = "Mor shala";
-        }
+
+        gameInitReply.PlayerMessage = request.PlayerCount is 2 or 4 ? $"Player count = {request.PlayerCount}" :
+            "Invalid Player Count!";
+
         return Task.FromResult(gameInitReply);
     }
+    
 }
+
